5b72f065b1f87e544eadf8c7718a10cb58e0360c
Matej1011 <mijanovic.matej@gmail.com>
2025-10-02 00:52:27 -0400

    finally a window is created

--- src/core/mini_window.h
+++ src/core/mini_window.h
@@ -4,19 +4,20 @@
 
 #include <memory>
 
-namespace mini_engine {
-	struct window_properties {
-		const char* title;
+namespace mini_engine
+{
+	struct window_properties
+	{
+		const char *title;
 		uint32_t width;
 		uint32_t height;
 
-		window_properties(const char* Title = "Mini Engine", uint32_t Width = 800, uint32_t Height = 600):
-			title(Title), width(Width), height(Height){}
+		window_properties(const char *Title = "Mini Engine", uint32_t Width = 800, uint32_t Height = 600) : title(Title), width(Width), height(Height) {}
 	};
-	class mini_window {
+	class mini_window
+	{
 
 	public:
-
 		virtual ~mini_window() {}
 		virtual void OnUpdate() = 0;
 		virtual void OnDraw() = 0;
@@ -24,8 +25,7 @@ namespace mini_engine {
 		virtual uint32_t GetWidth() const = 0;
 		virtual uint32_t GetHeight() const = 0;
 
-		static std::unique_ptr<mini_window> Create(const window_properties& props = window_properties());
-
+		static std::unique_ptr<mini_window> Create(const window_properties &props = window_properties());
 	};
 }
 
---
+++ src/platforms/Windows/WindowsWindow.h
@@ -0,0 +1,37 @@
+
+#ifndef WINDOWSWINDOW_H
+#define WINDOWSWINDOW_H
+
+#include "../../core/mini_window.h"
+
+namespace mini_engine
+{
+	class WindowsWindow : public mini_window
+	{
+
+	public:
+		WindowsWindow(const window_properties &windowProps);
+		WindowsWindow(const WindowsWindow &) = delete;
+		WindowsWindow &operator=(const WindowsWindow &) = delete;
+		virtual ~WindowsWindow() {}
+
+		virtual void OnUpdate() override;
+		virtual void OnDraw() override;
+		virtual void OnClose() override;
+
+		virtual uint32_t GetWidth() const override { return m_data.width; };
+		virtual uint32_t GetHeight() const override { return m_data.height; };
+
+	private:
+		bool init(const window_properties &windowProps);
+
+	private:
+		struct window_data
+		{
+			const char *title;
+			uint32_t width, height;
+		} m_data;
+	};
+}
+
+#endif
